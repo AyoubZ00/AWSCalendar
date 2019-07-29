944ab521505ddca442b242bea5327858f016387a
ayoub <ayoub.azougarh96@gmail.com>
2019-07-29 15:25:10 +0100

    modified the calendar template : the path to the entry pt

--- v0/src/calendar/main.py
+++ v0/src/calendar/main.py
@@ -1,7 +1,9 @@
 from ServiceCalendar.parsetojson import *
 import json
 import dateutil.parser
-def createCalendar(event, context):
+
+
+def GetCalendar(event, context):
         # yourdate = dateutil.parser.parse(adate)
     return {
         'body': json.loads(getOneTodayCalendarAPI(20))
@@ -10,10 +12,10 @@ def createCalendar(event, context):
     
 
 
-if __name__ == '__main__':
-    # adate = '2019-07-30T08:00:00Z'
-    # yourdate = dateutil.parser.parse(adate)
-    # event = {}
-    # event['date'] = '2019-07-30T08:00:00Z'
-    # print(createCalendar(event,'hey'))
-    print(createCalendar(1,'hey'))
+# if __name__ == '__main__':
+#     # adate = '2019-07-30T08:00:00Z'
+#     # yourdate = dateutil.parser.parse(adate)
+#     # event = {}
+#     # event['date'] = '2019-07-30T08:00:00Z'
+#     # print(createCalendar(event,'hey'))
+#     print(GetCalendar(1,'hey'))
